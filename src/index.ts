98124f1dcd45a172c9a8afabcdb8f6a5958be637
Kavindu Maleesha <kaviigunawardhana2000@gmail.com>
2024-08-01 19:17:52 +0530

    Port Update
    Fix dynamic port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import cors from "cors";
 import GlobalErrorHandlingMiddleware from "./api/middleware/global-error-handler";
 
 const app = express();
-
 app.use(express.json());
 app.use(cors())
 
@@ -18,5 +17,5 @@ app.use("/jobApplications", jobApplicationRouter)
 
 app.use(GlobalErrorHandlingMiddleware)
 
-const PORT = 8000
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
